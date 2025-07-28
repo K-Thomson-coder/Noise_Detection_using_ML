a3c1b6e88d40c5f8d5612b2e2d9e99c3d231c9a3
Kangujam Thomson Singh <thomsonkangujam05@gmail.com>
2025-07-28 09:39:44 +0530

    Update : requirement.txt | Add : Noise_Detector.py (Simple UI); models/model.pkl, scaler.pkl

---
+++ Noise_Detector.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import sounddevice as sd
+import numpy as np
+import librosa
+from scripts import file
+import tempfile
+import scipy.io.wavfile
+
+model = file.load_file("models/model.pkl")
+scaler = file.load_file("models/scaler.pkl")
+
+label_map = {0 : 'Silence', 1 : 'Noise'}
+
+st.title("Real-Time Noise Detector")
+
+duration = st.slider("Recording duration (seconds)", 1, 5, 3)
+
+if st.button("Record & Predict") :
+    st.write("Recording...")
+    fs = 22050 #sample rate
+    recording = sd.rec(int(duration * fs), samplerate=fs, channels=1)
+    sd.wait()
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as f :
+        scipy.io.wavfile.write(f.name, fs, recording)
+        file_path = f.name
+
+    st.audio(file_path, format="audio/wav")
+
+    try :
+        y, sr = librosa.load(file_path, sr=None)
+        mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+        mfccs_mean = np.mean(mfccs.T, axis=0).reshape(1, -1)
+
+        scaled_mfccs = scaler.transform(mfccs_mean)
+        
+        prediction = model.predict(scaled_mfccs)
+        result = label_map[int(prediction[0])]
+
+        st.success(f"Prediction : *{result}*")
+
+    except Exception as e :
+        st.error(f"Error processing audio : {e}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
